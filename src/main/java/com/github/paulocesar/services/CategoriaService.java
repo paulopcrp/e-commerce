7add9735a0847143295b4a5291fd739f0076a405
Paulo Cesar Rodrigues Pereira <paulo.pcrp@gmail.com>
2021-02-19 11:20:31 -0300

    Criado metodo POST para categoria

--- src/main/java/com/github/paulocesar/services/CategoriaService.java
+++ src/main/java/com/github/paulocesar/services/CategoriaService.java
@@ -22,6 +22,11 @@ public class CategoriaService {
 			  "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 			
 		}
+		
+		public Categoria inserir(Categoria obj) {
+			obj.setId(null);
+			return repositorio.save(obj);
+		}
 	
 }
 
