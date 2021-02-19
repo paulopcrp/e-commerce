fc52e81bab87cf9979dd9ba782bac486a069d720
Paulo Cesar Rodrigues Pereira <paulo.pcrp@gmail.com>
2021-02-19 09:45:19 -0300

    Endpoint/Pedidos/Id disponibilizados

--- src/main/java/com/github/paulocesar/domain/ItemPedido.java
+++ src/main/java/com/github/paulocesar/domain/ItemPedido.java
@@ -5,14 +5,18 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-		
+	
+	
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
@@ -30,6 +34,7 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
---
+++ src/main/java/com/github/paulocesar/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.github.paulocesar.services;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.paulocesar.domain.Pedido;
+import com.github.paulocesar.repositories.PedidoRepository;
+import com.github.paulocesar.services.excessao.ObjetoNaoEcontradoExcessao;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repositorio; //objeto repositorio
+	
+		public Pedido buscar(Integer id) {
+			 Optional<Pedido> obj = repositorio.findById(id);
+			 return obj.orElseThrow(() -> new ObjetoNaoEcontradoExcessao(
+			  "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+			
+		}
+	
+}
+
