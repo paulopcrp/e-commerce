f5d34d776f5b39e85cb3873bf2a9351748c0cd32
Paulo Cesar Rodrigues Pereira <paulo.pcrp@gmail.com>
2021-02-21 18:55:55 -0300

    paginação com parametros opcionais na requisição

--- src/main/java/com/github/paulocesar/services/CategoriaService.java
+++ src/main/java/com/github/paulocesar/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.github.paulocesar.domain.Categoria;
@@ -48,6 +51,11 @@ public class CategoriaService {
 		
 		public List<Categoria> findAll() {
 			return repositorio.findAll();
+		}
+		
+		public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+			return repositorio.findAll(pageRequest);
 		}
 	
 }
