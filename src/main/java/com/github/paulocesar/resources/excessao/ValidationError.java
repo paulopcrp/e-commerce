fbbac32118b829daaba90c742273c2efd43840bb
Paulo Cesar Rodrigues Pereira <paulo.pcrp@gmail.com>
2021-02-23 07:41:16 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/github/paulocesar/resources/excessao/ValidationError.java
@@ -0,0 +1,26 @@
+package com.github.paulocesar.resources.excessao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandartError {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+
+}
