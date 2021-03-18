3545f6b76718a171c131bcda938b5f0bfecadf75
Paulo Cesar Rodrigues Pereira <paulo.pcrp@gmail.com>
2021-03-18 15:52:17 -0300

    subtoal e total do pedido

--- src/main/java/com/github/paulocesar/domain/Pedido.java
+++ src/main/java/com/github/paulocesar/domain/Pedido.java
@@ -58,6 +58,14 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoEntrega = enderecoEntrega;
 	}
+	
+	public double getValotTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens ) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
 
 
 	public Integer getId() {
